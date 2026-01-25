8c5b6b61c3007e615fa7f59dd3c322bdd93f5e2a
attilabekecs <attila.bekecs@icloud.com>
2026-01-25 16:20:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzY2A_HgMJ9KVnokRI7yNpf1mpkPePIbzcZNRz9nrYYcBqbks0LFicpJpAaiOnOlJS6/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbysn8JVnLmeBIgdAn9o6b1MEm6pOJiuwF_jqhxUXENjC0sNGR8bVbI_8lRUXRxohBGz/exec"
 };
